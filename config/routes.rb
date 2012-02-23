7ce7cbf035db4e42e8f80ef21e3665d16b34a87c
Jonathan Liang <jonathan.liang@gmail.com>
2012-02-22 23:04:33 -0800

    rudimentary authentication w/ devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Saintstir::Application.routes.draw do
 
+  #//  Root points to singular HomesController
+  root :to => "homes#show"
 
-  #//  -- Administrative / Editor Modules --
+  #//  Define homepage as singular resource, since all ppl share the same homepage content
+  resource :home, :only => [:show]
 
+  #//  Generate devise-related routes (auth/login)
+  devise_for :users
+
+  #//  Administrative / Editor Modules
   #//  Saints Editor Module
   resources :saints
-
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111229044900) do
+ActiveRecord::Schema.define(:version => 20120221061522) do
 
   create_table "attrib_categories", :force => true do |t|
     t.string   "code",        :null => false
@@ -86,5 +86,23 @@ ActiveRecord::Schema.define(:version => 20111229044900) do
   end
 
   add_index "saints", ["symbol"], :name => "index_saints_on_symbol", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
