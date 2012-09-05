fdf40dbd97b44fe8d73e1bfb1cc19e9066dc9cb9
Jonathan Liang <jonathan.liang@gmail.com>
2012-09-04 23:53:43 -0700

    wikipedia bio scraper integration

--- config/initializers/cache_manager_config.rb
+++ config/initializers/cache_manager_config.rb
@@ -4,8 +4,9 @@ require 'cache_manager'
 #//  We'll be using the SaintstirCacheManager to manage all caching
 
 CacheManager.configure = [
-  CacheConfig.new(CacheConfig::PARTITION_SAINT_BLURB, CacheConfig::CACHE_TYPE_VIEW, true, false),
   CacheConfig.new(CacheConfig::PARTITION_SAINTS_ISOTOPE, CacheConfig::CACHE_TYPE_VIEW, true, true),
+  CacheConfig.new(CacheConfig::PARTITION_SAINT_BLURB, CacheConfig::CACHE_TYPE_VIEW, true, false),
+  CacheConfig.new(CacheConfig::PARTITION_SAINT_WIKI_BIO, CacheConfig::CACHE_TYPE_OBJ, true, false),
   CacheConfig.new(CacheConfig::PARTITION_FLICKR_SET, CacheConfig::CACHE_TYPE_OBJ)
 ]
 
