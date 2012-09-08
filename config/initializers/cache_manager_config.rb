02824c3f2a501c8a12f4f60a42d61a3c31f0b24c
Jonathan Liang <jonathan.liang@gmail.com>
2012-09-08 10:43:56 -0700

    Make flickr cache a managed cached tied to saint edits

--- config/initializers/cache_manager_config.rb
+++ config/initializers/cache_manager_config.rb
@@ -7,7 +7,7 @@ CacheManager.configure = [
   CacheConfig.new(CacheConfig::PARTITION_SAINTS_ISOTOPE, CacheConfig::CACHE_TYPE_VIEW, true, true),
   CacheConfig.new(CacheConfig::PARTITION_SAINT_BLURB, CacheConfig::CACHE_TYPE_VIEW, true, false),
   CacheConfig.new(CacheConfig::PARTITION_SAINT_WIKI_BIO, CacheConfig::CACHE_TYPE_OBJ, true, false),
-  CacheConfig.new(CacheConfig::PARTITION_FLICKR_SET, CacheConfig::CACHE_TYPE_OBJ)
+  CacheConfig.new(CacheConfig::PARTITION_FLICKR_SET, CacheConfig::CACHE_TYPE_OBJ, true, false)
 ]
 
 
