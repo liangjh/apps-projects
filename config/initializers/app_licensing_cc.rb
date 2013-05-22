4b0558ae728aec686d23babd7e3ebb993bc7cf0d
liangjh <liangjh@ip-192-168-1-105.ec2.internal>
2013-05-21 23:43:34 -0700

    Integrate licensing into all successful API calls

--- config/initializers/app_licensing_cc.rb
+++ config/initializers/app_licensing_cc.rb
@@ -3,9 +3,9 @@
 #  Constants related to licensing for the application
 module AppLicensing
 
-
   CC_LICENSE_LANG_URL = "http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"
   CC_LICENSE_ICON_URL = "http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"
+  CC_LICENSE_BLURB    = "Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License"
 
 end
 
