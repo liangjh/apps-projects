e24433b8206cbb22520124b8e1621915c7ad9809
liangjh <liangjh@ip-192-168-1-105.ec2.internal>
2013-05-21 23:23:55 -0700

    Add creativecommons license to footer for all pages

---
+++ config/initializers/app_licensing_cc.rb
@@ -0,0 +1,13 @@
+
+##
+#  Constants related to licensing for the application
+module AppLicensing
+
+
+  CC_LICENSE_LANG_URL = "http://creativecommons.org/licenses/by-nc-sa/3.0/deed.en_US"
+  CC_LICENSE_ICON_URL = "http://i.creativecommons.org/l/by-nc-sa/3.0/80x15.png"
+
+end
+
+
+
