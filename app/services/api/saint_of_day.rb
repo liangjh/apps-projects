f9bc96a4fe9273f956f8c86775809c242a2e5e64
liangjh <liangjh@JHL-4434.local>
2013-12-15 10:16:05 -0800

    Simplified saint of day algorithm + bug fixes

--- app/services/api/saint_of_day.rb
+++ app/services/api/saint_of_day.rb
@@ -1,8 +1,7 @@
-
-
 ##
 #  This retrieves the current saint of the day
-#  re-calculates the saint of day if necessary, but performs caching so th
+#  re-calculates the saint of day if necessary, but performs caching so
+#  that we don't need to re-calculate for each hit to saint of the day
 #
 class Api::SaintOfDay
 
@@ -16,36 +15,35 @@ class Api::SaintOfDay
     end
 
     def get_cached(effective_date = Date.today)
-      saint_featured_next_date = Setting.find_by_key(:saint_featured_next_date)
       saint_featured = Setting.find_by_key(:saint_featured)
 
-
       # If any of these values are nil, then re-derive
-      if saint_featured_next_date.value.nil? || Date.parse(saint_featured_next_date.value) < effective_date
-        next_mvs = yield effective_date
-        save_next(next_mvs, saint_featured, saint_featured_next_date, effective_date)
+      if saint_featured.updated_at.to_date < effective_date
+        next_mv = yield effective_date
+        saint_featured.update_attribute(:value, next_mv.saint_id.to_s)
       end
 
       #  Retrieve the actual featured saint
       Saint.find(saint_featured.value)
     end
 
+
     ##
     #  Retrieves all saints and derives the current and next saints to display
     def derive(effective_date)
-      this_yr = Date.today.year
       mvs = MetadataValue.where(:metadata_key_id => MetadataKey.find_by_code(:feastday))
+
+      #  Find the next day's set of saints
+      this_yr = Date.today.year
       sorted_mvs = mvs.sort { |x,y| Date.parse("#{this_yr}/#{x.value}") <=> Date.parse("#{this_yr}/#{y.value}") }
-      next_feasts = sorted_mvs.select { |x| Date.parse("#{this_yr}/#{x.value}") >= effective_date }.first(2)
-      next_feasts
+      next_feast_mv = sorted_mvs.find { |mv| Date.parse("#{this_yr}/#{mv.value}") >= effective_date }
+      next_feast_mv = sorted_mvs.first if next_feast_mv.nil?   # No more this year - start from beginning again
+
+      # All saints w/ same date as next_feast_mv - choose one at random
+      candidate_mvs = sorted_mvs.select { |mv| mv.value == next_feast_mv.value }
+      winner_mv = candidate_mvs[rand(candidate_mvs.length)]
     end
 
-    def save_next(next_mvs, saint_featured, saint_featured_next_date, effective_date)
-      saint_featured.value = next_mvs.size > 0 ? next_mvs[0].saint_id.to_s : Saint.first.id
-      saint_featured_next_date.value = next_mvs.size > 1 ? "#{effective_date.year}/#{next_mvs[1].value}" : effective_date.strftime("%Y/%m/%d")
-      saint_featured.save
-      saint_featured_next_date.save
-    end
   end
 
 
