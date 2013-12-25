3a61b06bbc42d33e9222a75ac4c75e7abce6d7ae
liangjh <liangjh@JHL-4434.local>
2013-12-25 12:43:24 -0800

    Number precision formatting, for all results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+
+  ##
+  #  Delegates to number_to_precision for now w/ a fixed percentage
+  #  but in the future this can be a custom setting on a user-by-user basis
+  def format_precision(number)
+    number_with_precision(number, :precision => 4)
+  end
+
 end
