5defc4160020d8bc7e77749737c91b68646ee288
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-08-20 19:22:04 -0500

    memoization decorator.  controller function, init

---
+++ app/caching/cache.py
@@ -0,0 +1,33 @@
+import types
+
+
+def memoize(func: types.FunctionType) -> types.FunctionType:
+    '''
+    Decorator for memoization / caching
+    Use this on functions w/ more intensive processing
+    Be judicious about this,  there is no cleanup
+
+    Parameters:
+    func (types.FunctionType): function that performs action for caching
+
+    '''
+    cache = {}
+    
+    def decorated(*args, **kwargs):
+        '''
+        If the key implied by the arg/kwarg combination already exists,
+        then return from cache; otherwise initialize and place into cache
+        '''
+
+        #  Assemble key for cache
+        argkey = '_'.join(args)
+        kwargkey = '_'.join(['{}:{}'.format(str(k), str(v)) for k,v in kwargs])
+        key = '{}__{}'.format(argkey, kwargkey)
+
+        if key not in cache.keys():
+            value = func(*args, **kwargs)
+            cache[key] = value
+        
+        return cache[key]
+
+    return decorated
---
+++ app/spire/inspire.py
@@ -0,0 +1,24 @@
+'''
+Inspire, entry point
+Main controller for inspire generation
+'''
+
+def inspire(params: dict) -> dict:
+    
+    # Retrieve markov model(s)
+    # Generate sensible quote (alt. between the two models)
+    # Retrieve image using unsplash integration
+    # Use existing image library (i.e. alt between dynamic search and general inspire)
+    # Draw quote over image (medium, small)
+    # Image size optimization
+    # Save images to data location
+    # Persist quote (+ image link) to persistent storage
+    # Usage logging  (if exists)
+    # Return image locations + metadata (in dict)
+    
+    pass
+
+
+
+
+
