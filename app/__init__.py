b847099765a426cddc7dc705a06482973dabcc68
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-08-21 20:47:23 -0500

    load config bacsed on environment setting

--- app/__init__.py
+++ app/__init__.py
@@ -6,25 +6,19 @@ from .api import api
 #  Initialize flask app / server
 app = Flask(__name__)
 
-#  Load environment variables 
-app.config.from_object('config.default')
-app.config.from_object('config.development')
-
 #  Load blueprints
 app.register_blueprint(api)
 
 #  Enviroment initialization
+#  Pass variable "FLASK_ENV", or will assume development
 envobj = {
-    'development': 'config.development',
-    'staging': 'config.staging',
-    'production': 'config.production'
+    'development':  'config.development',
+    'staging':      'config.staging',
+    'production':   'config.production'
 }[os.environ.get('FLASK_ENV', 'development')]
 
 print('Loading environments: {}'.format(', '.join(['config.default', envobj])))
 app.config.from_object('config.default')
 app.config.from_object(envobj)
 
-
-
-#  TODO: toggle environment to load
 
