8c9706865786cefeb88bbb5f050732c2165a3e39
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-08-25 19:05:13 -0500

    change location of raw image save

--- config/development.py
+++ config/development.py
@@ -1,5 +1,5 @@
 
 # Data directory (i.e. models, etc) 
 MODEL_DIRECTORY='/Users/liangjh/Workspace/tspire-api/data/model/'
-IMAGE_DIRECTORY='/Users/liangjh/Workspace/tspire-api/data/image/'
+IMAGE_DIRECTORY='/Users/liangjh/Workspace/tspire-api/data/images/raw/'
 DATA_DIRECTORY='/Users/liangjh/Workspace/tspire-api/data/'
