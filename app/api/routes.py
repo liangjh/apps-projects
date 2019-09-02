ce7c82130e819155892812804ce67ebd2f047962
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-09-02 12:07:43 -0500

    file upload functionality to raw directory folder

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,6 @@
-from flask import jsonify, request, current_app
+import os
+from flask import jsonify, request, current_app, redirect, url_for, flash
+from werkzeug.utils import secure_filename
 from . import api
 from app.spire import inspire
 
@@ -39,6 +41,36 @@ def api_search():
 
     results = inspire.inspire_search(persona, q, current_app.config)
     return jsonify(results)
+
+
+#  NOTE: this is to allow upload of files to file system
+@api.route('/upload', methods=['GET','POST'])
+def api_upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        else:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['RAW_UPLOAD_FOLDER'], filename))
+    
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+        <input type=file name=file>
+        <input type=submit value=Upload>
+        </form>
+    '''
 
 
 # TODO: enable this for development, otherwise comment out
--- config/development.py
+++ config/development.py
@@ -8,7 +8,7 @@ IMAGE_RAW_DIRECTORY = '/Users/liangjh/Workspace/tspire-api/data/images/raw/'
 IMAGE_GEN_DIRECTORY = '/Users/liangjh/Workspace/tspire-api/data/images/generated/'
 IMAGE_SERVER_BASE = 'http://localhost:8081/'
 
-
+RAW_UPLOAD_FOLDER = '/Users/liangjh/Workspace/tspire-api/data/'
 
 #  Connections, by key
 DATABASE_CONNECTIONS = {
