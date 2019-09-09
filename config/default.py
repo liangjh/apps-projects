a450422cc9bf752307441d2196bfa3acfa47100d
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-09-08 21:07:58 -0500

    remove extraneous model file refs

--- config/default.py
+++ config/default.py
@@ -5,8 +5,6 @@ PERSONAS = ['Trump']
 # Trump corpus calibrated markov models - based on (1) speeches, (2) tweets
 MARKOV_MODELS = {
     'Trump': [
-        # {'name': 'model-speech', 'filename': 'trumpspire-model-01.json', 'max_chars': 200},
-        # {'name': 'model-tweet',  'filename': 'trumpspire-model-02.json', 'max_chars': 150}
         {'name': 'model-speech', 'filename': 'trumpspire-model-01.gz', 'max_chars': 200},
         {'name': 'model-tweet',  'filename': 'trumpspire-model-02.gz', 'max_chars': 150}
     ]
