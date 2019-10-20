06721146b0b32119fc6afda941ce5bad179cdf0e
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-10-20 07:36:40 -0500

    shuffle sentence extraction rulset

--- app/spire/inspire.py
+++ app/spire/inspire.py
@@ -20,7 +20,7 @@ def inspire_generate(persona: str='Trump', config: dict={}) -> dict:
     # Retrieve markov model(s)
     # Generate sensible quote (alt. between the two models)
     text = spiremodel.markov_generate(persona, config)
-    topics = spiremodel.sentence_topic_extract(text, config['TOPIC_PARSE_RULES'])
+    topics = spiremodel.sentence_topic_extract(text, random.shuffle(config['TOPIC_PARSE_RULES'])) # randomize ruleset
     title = topics[0].text if len(topics) > 0 else None
 
     # TODO: pixabay dynamic search may not be needed
