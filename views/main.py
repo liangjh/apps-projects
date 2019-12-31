a108ec3d10e1c5b78edc18610c5284ff4deefe9b
Jonathan Liang <jonathan.h.liang@gmail.com>
2019-12-31 16:32:15 -0600

    initial commit; using assembly-flask framework

---
+++ views/main.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Assembly views.py
+"""
+
+from assembly import (Assembly,
+                      asm,
+                      date,
+                      models,
+                      request,
+                      response,
+                      HTTPError)
+
+# ------------------------------------------------------------------------------
+
+
+
+class Api(Assembly):
+
+    @request.cors
+    @response.json
+    def calculate(self):
+        return {
+            "request_id": "abcd",
+            "data": "xyz"
+        }
+
+
+class Index(Assembly):
+
+    def index(self):
+        return
+
+    @request.cors
+    @response.json
+    def api(self):
+        return {
+            "date": date.utcnow(),
+            "description": "API Endpoint with CORS and JSON response"
+        }
+
+    @response.json
+    @response.cache(timeout=10)
+    def cached(self):
+        return {
+            "description": "This is a cached endpoint",
+            "date": date.utcnow(),
+        }
+
+
+    def error(self):
+        """
+        Accessing /error should trigger the error handlers below
+        """
+        raise HTTPError.NotFound()
