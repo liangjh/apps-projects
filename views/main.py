d09998d7908152a7d2bb43a784e541ef178e8dea
Jonathan Liang <jonathan.h.liang@gmail.com>
2020-02-01 20:08:22 -0600

    completion, API endpoints + err handling

--- views/main.py
+++ views/main.py
@@ -2,29 +2,76 @@
 """
 Assembly views.py
 """
+import json
+import importlib.resources as pkg_resources
 
 from assembly import (Assembly,
-                      asm,
                       date,
-                      models,
                       request,
                       response,
                       HTTPError)
 
+from assembly import config as asmbl_config
+from lib.tweemio import twm
+import resources as asmbl_resources
+
 # ------------------------------------------------------------------------------
 
 
-
+#  Primary API endpoint / gateway for tweemio functionality
 class Api(Assembly):
 
     @request.cors
     @response.json
     def calculate(self):
-        return {
-            "request_id": "abcd",
-            "data": "xyz"
-        }
+        '''
+        Performs similarity calculation for a given screen_name against a tweem.io preconfigured group
+        '''
 
+        screen_name = request.args.get('screen_name')
+        group = request.args.get('group')
+        force = request.args.get('force') == 'true'
+
+        # Basic error / parameter checking
+        if (screen_name is None or group is None):
+            return generate_api_error('Missing one or more required parameters: screen_name, group')
+        group_set = list(asmbl_config['SIMILARITY_COMPARISONS'].keys())
+        if (group not in group_set):
+            return generate_api_error(f'Group must be one of the following: {group_set}')
+
+        # Invoke calculation gateway and return
+        results = twm.calculate(asmbl_config, screen_name, group, force)
+        return results
+
+
+    @request.cors
+    @response.json
+    def groupmeta(self):
+        '''
+        Meta-data for each group;  description, screen names within each group to be evaluated
+        '''
+        return asmbl_config['SIMILARITY_COMPARISONS']
+
+
+    @request.cors
+    @response.json
+    def screenmeta(self):
+        '''
+        Meta-data for each screen name in sysstem
+        '''
+        screen_meta_txt  = pkg_resources.read_text(asmbl_resources, 'model_user_profiles.json')
+        screen_meta_json = json.loads(screen_meta_txt)
+        return screen_meta_json
+
+
+def generate_api_error(msg: str):
+    return {
+        'error': True,
+        'message': msg
+    }
+
+
+#  --------------------  Testing / Evaluation ---------------------------
 
 class Index(Assembly):
 
